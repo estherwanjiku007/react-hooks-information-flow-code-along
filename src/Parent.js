dcc961bc81477c74cb0be2806034c7c700af83a8
ESTHER WANJIKU <ESTHERWANJIQU@GMAIL.COM>
2024-02-01 09:33:44 +0300

    aster

--- src/Parent.js
+++ src/Parent.js
@@ -5,11 +5,34 @@ import Child from "./Child";
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
-
+  const [childrenColor,setChildrenColor]=useState("#FFF")
+  function handleNewColor(){
+    const randomColor2=getRandomColor()
+    setColor(randomColor2)
+  }
+  function handleChildColor(newChildColor){
+    const newRandomColor=getRandomColor()
+    setColor(newRandomColor)
+    setChildrenColor(newChildColor)
+  }
+  function Child({ onChangeColor, color }) {
+    function handleClick() {
+      const newColor = getRandomColor();
+      onChangeColor(newColor);
+    }
+  
+    return (
+      <div
+        onClick={handleClick}
+        className="child"
+        style={{ backgroundColor: color }}
+      />
+    );
+  }
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child onChangeColor={handleNewColor} color={handleChildColor}/>
+      <Child  onChangeColor={handleNewColor} color={handleChildColor}/>
     </div>
   );
 }
